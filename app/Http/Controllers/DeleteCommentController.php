f0bd20fa40819502b18163e24429e48ed3cb6590
mostafa hajr <mostafahajr123@gmail.com>
2024-10-17 05:46:12 +0300

    third commit

--- app/Http/Controllers/DeleteCommentController.php
+++ app/Http/Controllers/DeleteCommentController.php
@@ -7,12 +7,14 @@ use Illuminate\Http\Request;
 
 class DeleteCommentController extends Controller
 {
+
     /**
      * Handle the incoming request.
      */
     public function __invoke($id,Comment $comment)
     {
         //
+
         $comment->delete();
         return back();
     }
--- resources/views/eventsShow.blade.php
+++ resources/views/eventsShow.blade.php
@@ -24,7 +24,7 @@
                         eventLike: @js($like),
                         savedEvent: @js($savedEvent),
                         attending: @js($attending),
-                       
+
                     }">
 
                         <form action="{{route('events.like',$event->id)}}" method="post">
@@ -130,7 +130,7 @@
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                 name="content" id="content" placeholder="Comment">
                             <button type="submit"
-                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                class=" bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Post
                             </button>
                         </form>
@@ -149,16 +149,16 @@
                                             <h2 class="text-lg font-bold text-gray-900">{{ $comment->user->name }}</h2>
                                         </div>
                                         <p class="text-sm font-semibold text-gray-400">{{ $comment->content }}</p>
-                                        @can('view', $comment)
+
                                             <form action="{{ route('events.comments.destroy', [$event->id, $comment->id]) }}"
                                                 method="POST">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button
-                                                    class="mt-6 rounded-lg bg-red-400 px-4 py-2 text-sm tracking-wider text-white outline-none hover:bg-red-300">Delete</button>
+                                                    class="mt-6 rounded-lg bg-red-400 px-4 py-2 text-sm tracking-wider  outline-none hover:bg-red-300">Delete</button>
 
                                             </form>
-                                        @endcan
+
                                     </div>
                                 </div>
                             </div>
